bb1ceeb7f1db30a3aa52fe30b4bfeb01f3512d71
UrBoiRAD <rickadaniel22@gmail.com>
2023-07-06 16:08:50 -0500

    Minor Update (7/6/23)
    Updated project to Godot 4.1
    Updated DiscordSDK plugin

--- src/addons/discord-sdk-gd/nodes/core_updater.gd
+++ src/addons/discord-sdk-gd/nodes/core_updater.gd
@@ -1,18 +1,23 @@
-## This always needs to run in the background if you want to comunicate with the discord client.
+## [color=yellow]PLEASE IGNORE![/color] This is a important Node wich gets automatically added as Singleton.
+## 
+## The DiscordSDKLoader Node automatically gets added as Singleton while installing the addon.
+## It has to run in the background to comunicate with Discord.
+## You don't need to use it.
+##
+## @tutorial: https://github.com/vaporvee/discord-sdk-godot/wiki
+class_name core_updater
 extends Node
-
-@export var is_running: bool =false
 
 func _ready():
 	discord_sdk.app_id = 1109157117794988092 # Application ID
 	print("Discord working: " + str(discord_sdk.get_is_discord_working())) # A boolean if everything worked
-	discord_sdk.details = "Project Rapid SAGE 2023 Demo"
+	discord_sdk.details = "SAGE '23 Demo"
 	discord_sdk.state = "Playing"
 	
 	discord_sdk.large_image = "pricon" # Image key from "Art Assets"
 	discord_sdk.large_image_text = "TIME IS RUNNING OUT!"
-	discord_sdk.small_image = "boss" # Image key from "Art Assets"
-	discord_sdk.small_image_text = "Fighting the end boss! D:"
+	discord_sdk.small_image = "logosmallhd" # Image key from "Art Assets"
+	discord_sdk.small_image_text = ""
 
 	discord_sdk.start_timestamp = int(Time.get_unix_time_from_system()) # "02:46 elapsed"
 	# discord_sdk.end_timestamp = int(Time.get_unix_time_from_system()) + 3600 # +1 hour in unix time / "01:00 remaining"
@@ -21,4 +26,3 @@ func _ready():
 
 func _process(delta) -> void:
 	discord_sdk.coreupdate()
-	is_running = true
